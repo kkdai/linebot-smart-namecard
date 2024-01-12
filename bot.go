0fa707f036eccf80cd256467d281815900b4de66
Evan Lin <evan.if.lin@linecorp.com>
2024-01-12 21:29:36 +0800

    refactor: refactor callbackHandler function and remove unused code
    - Remove unused imports and constants
    - Change the prompt for the ImagePrompt constant
    - Remove the handleCameraQuickReply function
    - Change the prompt for the GeminiImage function
    - Remove the QuickReply buttons and modify the reply message for the ImageMessageContent case in the callbackHandler function
    - Remove the PostbackEvent case in the callbackHandler function
    - Add a JoinEvent case in the callbackHandler function
    
    Signed-off-by: Evan Lin <evan.if.lin@linecorp.com>

--- bot.go
+++ bot.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 
 	"github.com/line/line-bot-sdk-go/v7/linebot"
@@ -14,13 +12,7 @@ import (
 )
 
 // Const variables of Prompts.
-const ImagePrompt = "你是一個美食烹飪專家，根據這張圖片給予相關的食物敘述，越詳細越好。"
-const CalcPrompt = "根據這張圖片，幫我計算食物的卡路里。"
-const CookPrompt = "根據這張圖片，幫我找到相關的食譜。"
-
-// Image statics link.
-const CalcImg = "https://raw.githubusercontent.com/kkdai/linebot-smart-namecard/main/img/calc.jpg"
-const CookImg = "https://raw.githubusercontent.com/kkdai/linebot-smart-namecard/main/img/cooking.png"
+const ImagePrompt = "這是一張名片，你是一個名片秘書。請將以下資訊整理成 json 給我 Name, Title, Address, Email, Phone Number"
 
 // replyText: Reply text message to LINE server.
 func replyText(replyToken, text string) error {
@@ -39,33 +31,11 @@ func replyText(replyToken, text string) error {
 	return nil
 }
 
-func handleCameraQuickReply(replyToken string) error {
-	msg := &messaging_api.TextMessage{
-		Text: "請上傳一張美食照片，開始相關功能吧！",
-		QuickReply: &messaging_api.QuickReply{
-			Items: []messaging_api.QuickReplyItem{
-				{
-					ImageUrl: "",
-					Action: &messaging_api.CameraAction{
-						Label: "Camera",
-					},
-				},
-			},
-		},
-	}
-	if _, err := bot.ReplyMessage(
-		&messaging_api.ReplyMessageRequest{
-			ReplyToken: replyToken,
-			Messages:   []messaging_api.MessageInterface{msg},
-		},
-	); err != nil {
-		return err
-	}
-	return nil
-}
-
 // callbackHandler: Handle callback from LINE server.
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	card_prompt := os.Getenv("CARD_PROMPT")
+	channelToken = os.Getenv("ChannelAccessToken")
+
 	cb, err := webhook.ParseRequest(os.Getenv("ChannelSecret"), r)
 	if err != nil {
 		if err == linebot.ErrInvalidSignature {
@@ -83,22 +53,12 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			switch message := e.Message.(type) {
 			// Handle only on text message
 			case webhook.TextMessageContent:
-				// Resceive text message, reply with QuickReply buttons.
-				err := handleCameraQuickReply(e.ReplyToken)
-				if err != nil {
-					log.Print(err)
-				}
+				log.Printf("Got text message, ID: %s, text: %s", message.Id, message.Text)
+
 			// Handle only on Sticker message
 			case webhook.StickerMessageContent:
-				var kw string
-				for _, k := range message.Keywords {
-					kw = kw + "," + k
-				}
-
-				outStickerResult := fmt.Sprintf("收到貼圖訊息: %s, pkg: %s kw: %s  text: %s", message.StickerId, message.PackageId, kw, message.Text)
-				if err := replyText(e.ReplyToken, outStickerResult); err != nil {
-					log.Print(err)
-				}
+				// log sticker id and package id.
+				log.Printf("Got sticker message, packageID: %s, stickerID: %s", message.PackageId, message.StickerId)
 
 			// Handle only image message
 			case webhook.ImageMessageContent:
@@ -111,32 +71,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					continue
 				}
 
-				ret, err := GeminiImage(data, ImagePrompt)
+				ret, err := GeminiImage(data, card_prompt)
 				if err != nil {
 					ret = "無法辨識影片內容文字，請重新輸入:" + err.Error()
-				}
-
-				// Prepare QuickReply buttons.
-				qReply := &messaging_api.QuickReply{
-					Items: []messaging_api.QuickReplyItem{
-						{
-							ImageUrl: CalcImg,
-							Action: &messaging_api.PostbackAction{
-								Label:       "calc",
-								Data:        "action=calc&m_id=" + message.Id,
-								DisplayText: "",
-								Text:        "計算卡路里",
-							},
-						}, {
-							ImageUrl: CookImg,
-							Action: &messaging_api.PostbackAction{
-								Label:       "cook",
-								Data:        "action=cook&m_id=" + message.Id,
-								DisplayText: "",
-								Text:        "建議食譜",
-							},
-						},
-					},
 				}
 
 				// Determine the push msg target.
@@ -145,8 +82,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 						ReplyToken: e.ReplyToken,
 						Messages: []messaging_api.MessageInterface{
 							&messaging_api.TextMessage{
-								Text:       ret,
-								QuickReply: qReply,
+								Text: ret,
 							},
 						},
 					},
@@ -162,35 +98,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				log.Printf("Unknown message: %v", message)
 			}
 		case webhook.PostbackEvent:
-			// Using urls value to parse event.Postback.Data strings.
-			ret, err := url.ParseQuery(e.Postback.Data)
-			if err != nil {
-				log.Print("action parse err:", err, " dat=", e.Postback.Data)
-				continue
-			}
-
-			log.Println("Action:", ret["action"])
-			log.Println("Calc calories m_id:", ret["m_id"])
-
-			// 取得用戶 ID
-			var target string
-			switch source := e.Source.(type) {
-			case *webhook.UserSource:
-				target = source.UserId
-			case *webhook.GroupSource:
-				target = source.UserId
-			case *webhook.RoomSource:
-				target = source.UserId
-			}
-
-			// Handle only on Postback message
-			if ret["action"][0] == "calc" {
-				// Determine the push msg target.
-				go processImage(target, ret["m_id"][0], CalcPrompt, "GeminiImage", blob) // for calcCalories
-			} else if ret["action"][0] == "cook" {
-				// Determine the push msg target.
-				go processImage(target, ret["m_id"][0], CookImg, "GeminiImage", blob) // for searchCooking
-			}
+			log.Printf("Got postback: %v", e.Postback.Data)
+		case webhook.JoinEvent:
+			log.Printf("Got join event")
 		case webhook.FollowEvent:
 			log.Printf("message: Got followed event")
 		case webhook.BeaconEvent:

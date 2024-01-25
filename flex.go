79a0d877d032c1aaac2dc17e516fa554f40f9f16
Evan Lin <evan.if.lin@linecorp.com>
2024-01-25 22:49:05 +0800

    Refactor flex message structure

--- flex.go
+++ flex.go
@@ -2,127 +2,7 @@ package main
 
 import "github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
 
-//  {
-//  "type": "carousel",
-// 	"contents": [
-// 		{
-// 			"type": "bubble",
-// 			"size": "giga",
-// 			"body": {
-// 				"layout": "horizontal",
-// 				"spacing": "lg",
-// 				"type": "box",
-// 				"contents": [
-// 					{
-// 						"layout": "vertical",
-// 						"type": "box",
-// 						"width": "100px",
-// 						"contents": [
-// 							{
-// 								"flex": 1,
-// 								"layout": "vertical",
-// 								"type": "box",
-// 								"contents": [
-// 									{
-// 										"type": "filler"
-// 									}
-// 								]
-// 							},
-// 							{
-// 								"height": "100px",
-// 								"layout": "vertical",
-// 								"type": "box",
-// 								"width": "100px",
-// 								"contents": [
-// 									{
-// 										"align": "center",
-// 										"aspectMode": "cover",
-// 										"aspectRatio": "1:1",
-// 										"gravity": "center",
-// 										"type": "image",
-// 										"url": "https://raw.githubusercontent.com/kkdai/linebot-smart-namecard/main/img/logo.jpeg"
-// 									}
-// 								]
-// 							},
-// 							{
-// 								"flex": 1,
-// 								"layout": "vertical",
-// 								"type": "box",
-// 								"contents": [
-// 									{
-// 										"type": "filler"
-// 									}
-// 								]
-// 							}
-// 						]
-// 					},
-// 					{
-// 						"borderColor": "#6EC4C4",
-// 						"borderWidth": "1px",
-// 						"flex": 0,
-// 						"height": "120px",
-// 						"layout": "vertical",
-// 						"type": "box",
-// 						"contents": [
-// 							{
-// 								"type": "filler"
-// 							}
-// 						]
-// 					},
-// 					{
-// 						"flex": 3,
-// 						"layout": "vertical",
-// 						"type": "box",
-// 						"contents": [
-// 							{
-// 								"flex": 1,
-// 								"layout": "vertical",
-// 								"type": "box",
-// 								"contents": [
-// 									{
-// 										"type": "filler"
-// 									}
-// 								]
-// 							},
-// 							{
-// 								"color": "#6EC4C4",
-// 								"size": "sm",
-// 								"text": "Company",
-// 								"type": "text",
-// 								"weight": "bold"
-// 							},
-// 							{
-// 								"color": "#81C997",
-// 								"margin": "xxl",
-// 								"size": "xxs",
-// 								"type": "text",
-// 								"text": "Title"
-// 							},
-// 							{
-// 								"color": "#81C997",
-// 								"size": "xl",
-// 								"text": "Name",
-// 								"type": "text",
-// 								"weight": "bold"
-// 							},
-// 							{
-// 								"type": "text",
-// 								"text": "address"
-// 							},
-// 							{
-// 								"type": "text",
-// 								"text": "email"
-// 							},
-// 							{
-// 								"type": "text",
-// 								"text": "phone"
-// 							}
-// 						]
-// 					}
-// 				]
-// 			}
-// 		},
-
+// SendFlexMsg: Send flex message to LINE server.
 func SendFlexMsg(replyToken string) error {
 	contents := &messaging_api.FlexCarousel{
 		Contents: []messaging_api.FlexBubble{
@@ -166,100 +46,61 @@ func SendFlexMsg(replyToken string) error {
 								},
 							},
 						},
+						&messaging_api.FlexBox{
+							BorderColor: "#6EC4C4",
+							BorderWidth: "1px",
+							Flex:        0,
+							Height:      "120px",
+							Layout:      messaging_api.FlexBoxLAYOUT_VERTICAL,
+							Contents: []messaging_api.FlexComponentInterface{
+								&messaging_api.FlexFiller{},
+							},
+						},
+						&messaging_api.FlexBox{
+							Flex:   3,
+							Layout: messaging_api.FlexBoxLAYOUT_VERTICAL,
+							Contents: []messaging_api.FlexComponentInterface{
+								&messaging_api.FlexBox{
+									Flex:   1,
+									Layout: messaging_api.FlexBoxLAYOUT_VERTICAL,
+									Contents: []messaging_api.FlexComponentInterface{
+										&messaging_api.FlexFiller{},
+									},
+								},
+								&messaging_api.FlexText{
+									Color:  "#6EC4C4",
+									Size:   "sm",
+									Text:   "Company",
+									Weight: "bold",
+								},
+								&messaging_api.FlexText{
+									Color:  "#81C997",
+									Margin: "xxl",
+									Size:   "xxs",
+									Text:   "Title",
+								},
+								&messaging_api.FlexText{
+									Color:  "#81C997",
+									Size:   "xl",
+									Text:   "Name",
+									Weight: "bold",
+								},
+								&messaging_api.FlexText{
+									Text: "address",
+								},
+								&messaging_api.FlexText{
+									Text: "email",
+								},
+								&messaging_api.FlexText{
+									Text: "phone",
+								},
+							},
+						},
 					},
 				},
 			},
 		},
 	}
-
-	// 									{
-	// 										"align": "center",
-	// 										"aspectMode": "cover",
-	// 										"aspectRatio": "1:1",
-	// 										"gravity": "center",
-	// 										"type": "image",
-	// 										"url": "https://raw.githubusercontent.com/kkdai/linebot-smart-namecard/main/img/logo.jpeg"
-	// 									}
-	// 								]
-	// 							},
-	// 							{
-	// 								"flex": 1,
-	// 								"layout": "vertical",
-	// 								"type": "box",
-	// 								"contents": [
-	// 									{
-	// 										"type": "filler"
-	// 									}
-	// 								]
-	// 							}
-	// 						]
-	// 					},
-	// 					{
-	// 						"borderColor": "#6EC4C4",
-	// 						"borderWidth": "1px",
-	// 						"flex": 0,
-	// 						"height": "120px",
-	// 						"layout": "vertical",
-	// 						"type": "box",
-	// 						"contents": [
-	// 							{
-	// 								"type": "filler"
-	// 							}
-	// 						]
-	// 					},
-	// 					{
-	// 						"flex": 3,
-	// 						"layout": "vertical",
-	// 						"type": "box",
-	// 						"contents": [
-	// 							{
-	// 								"flex": 1,
-	// 								"layout": "vertical",
-	// 								"type": "box",
-	// 								"contents": [
-	// 									{
-	// 										"type": "filler"
-	// 									}
-	// 								]
-	// 							},
-	// 							{
-	// 								"color": "#6EC4C4",
-	// 								"size": "sm",
-	// 								"text": "Company",
-	// 								"type": "text",
-	// 								"weight": "bold"
-	// 							},
-	// 							{
-	// 								"color": "#81C997",
-	// 								"margin": "xxl",
-	// 								"size": "xxs",
-	// 								"type": "text",
-	// 								"text": "Title"
-	// 							},
-	// 							{
-	// 								"color": "#81C997",
-	// 								"size": "xl",
-	// 								"text": "Name",
-	// 								"type": "text",
-	// 								"weight": "bold"
-	// 							},
-	// 							{
-	// 								"type": "text",
-	// 								"text": "address"
-	// 							},
-	// 							{
-	// 								"type": "text",
-	// 								"text": "email"
-	// 							},
-	// 							{
-	// 								"type": "text",
-	// 								"text": "phone"
-	// 							}
-	// 						]
-	// 					}
-	// 				]
-	// 			}
-	// 		},
 
 	if _, err := bot.ReplyMessage(
 		&messaging_api.ReplyMessageRequest{
